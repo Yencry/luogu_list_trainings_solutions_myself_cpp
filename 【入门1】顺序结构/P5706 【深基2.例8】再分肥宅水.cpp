ff2fb7f6eeaf50f6e37d588081d4da7811133288
Yencry <yencry@qq.com>
2025-06-01 13:53:15 +0800

    Add files via upload

---
+++ 【入门1】顺序结构/P5706 【深基2.例8】再分肥宅水.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main() {
+	double t; int n;
+	std::cin >> t >> n;
+	std::cout << std::fixed << std::setprecision(3) << static_cast<double>(t) / n << std::endl << n * 2;
+}
