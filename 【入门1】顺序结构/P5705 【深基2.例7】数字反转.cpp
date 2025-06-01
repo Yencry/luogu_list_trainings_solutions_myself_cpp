054282d06b3ce73ad4142e681f30c09f443257d6
Yencry <yencry@qq.com>
2025-06-01 12:31:13 +0800

    Add files via upload

---
+++ 【入门1】顺序结构/P5705 【深基2.例7】数字反转.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+	std::string input;
+	std::getline(std::cin, input);
+	std::reverse(input.begin(), input.end());//使用 std::reverse 必须显式包含 <algorithm> 头文件
+	double num = std::stof(input);
+	std::cout << num;
+}
