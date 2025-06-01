78791f09700fbfc610ebec1a33fb504feff59c23
莫尔加娜 <yencry@qq.com>
2025-06-01 19:48:12 +0800

    Create P5710 【深基3.例2】数的性质.cpp

---
+++ 【入门2】分支结构/P5710 【深基3.例2】数的性质.cpp
@@ -0,0 +1,35 @@
+﻿//#include <iostream>
+//
+//int main() {
+//	int m;
+//	std::cin >> m;
+//	if (m % 2 == 0 && m > 4 && m <= 12) std::cout << 1 << ' ';
+//	else std::cout << 0 << ' ';
+//
+//	if (m % 2 == 0 || (m > 4 && m <= 12)) std::cout << 1 << ' ';
+//	else std::cout << 0 << ' ';
+//
+//	if (m % 2 == 0 && (m <= 4 || m > 12)) std::cout << 1 << ' ';
+//	else if (m % 2 != 0 && (m > 4 && m <= 12)) std::cout << 1 << ' ';
+//	else std::cout << 0 << ' ';
+//
+//	if (m % 2 != 0 && (m <= 4 || m > 12)) std::cout << 1;
+//	else std::cout << 0;
+//
+//	return 0;
+//}
+
+#include <iostream>
+
+int main() {
+	int num	;
+	std::cin  >> num;
+	bool prop1 = num % 2 == 0;
+	bool prop2 = num > 4 && num <= 12;
+
+	std::cout << (prop1 && prop2 ? 1 : 0) << ' ';
+	std::cout << (prop1 || prop2 ? 1 : 0) << ' ';
+	std::cout << (prop1 != prop2 ? 1 : 0) << ' ';
+	std::cout << (!prop1 && !prop2 ? 1 : 0);
+	return 0;
+}
