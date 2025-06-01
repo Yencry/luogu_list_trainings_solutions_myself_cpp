94d45ecacfaefb854eef10ae2673eabd18ae5516
莫尔加娜 <yencry@qq.com>
2025-06-01 21:10:48 +0800

    update

---
+++ 【入门2】分支结构/P5712 【深基3.例4】Apples.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	int num;
+	std::cin >> num;
+	std::string word = num > 1 ? "apples":"apple";
+	std::cout << "Today, I ate " << num << " " << word << '.';
+	return 0;
+}
---
+++ 【入门2】分支结构/P5715 【深基3.例8】三位数排序.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	int e = a, f = b, g = c;
+	for (int i = 0; i < 3; i++) {
+		if (e > f) {
+			int temp = e;
+			e = f;
+			f = temp;
+		}
+		if (f > g) {
+			int temp = f;
+			f = g;
+			g = temp;
+		}
+	}
+	std::cout << e << " " << f << " " << g << std::endl;
+	return 0;
+}
