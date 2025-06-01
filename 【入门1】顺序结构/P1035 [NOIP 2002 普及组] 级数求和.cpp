012254d024e86ba553702b695a313b572c4694b3
Yencry <yencry@qq.com>
2025-06-01 12:23:03 +0800

    Add files via upload

---
+++ 【入门1】顺序结构/P1035 [NOIP 2002 普及组] 级数求和.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	double k;
+	std::cin >> k;
+	double sum = 0;	long long deno = 1;
+	while (true) {
+		sum += 1.0 / deno;
+		if (sum > k) {
+			std::cout << deno;
+			break;
+		}
+		deno++;
+	}
+	return 0;
+}
