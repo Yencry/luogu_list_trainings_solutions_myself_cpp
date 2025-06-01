35e36585bcec5858903609f9552aa3a9fd645c19
莫尔加娜 <yencry@qq.com>
2025-06-01 23:47:42 +0800

    update

---
+++ 【入门2】分支结构/P1085 [NOIP 2004 普及组] 不高兴的津津.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+
+int main() {
+	int hourSchool, hourAfter;
+	std::vector<int> allNumbers;
+	for (int i = 0, numLines = 7; i < numLines; ++i) {
+		std::cin >> hourSchool >> hourAfter;
+		allNumbers.push_back(hourSchool);
+		allNumbers.push_back(hourAfter);
+	}
+
+	std::array<int, 7> sadMood = {};
+	for (int i = 0; i < 7; i++)
+	{
+		int day = i * 2;
+		if (allNumbers[day] + allNumbers[day + 1] > 8)
+		{
+			sadMood[i] = allNumbers[day] + allNumbers[day + 1] - 8;
+		}
+		else sadMood[i] = 0;
+	}
+
+	auto maxIt = std::max_element(sadMood.begin(), sadMood.end());
+	if (std::all_of(sadMood.begin(), sadMood.end(), [](int x) { return x == 0; })) {
+		std::cout << 0 << std::endl;
+	}
+	else
+		std::cout << maxIt - sadMood.begin() + 1 << std::endl;
+	return 0;
+}
---
+++ 【入门2】分支结构/P5716 【深基3.例9】月份天数.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int main() {
+	int year, month, FebruaryDay = 28;
+	std::cin >> year >> month;
+	bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+	if (isLeapYear) FebruaryDay = 29;
+	switch (month) {
+	case 1:
+		std::cout << "31";
+		break;
+	case 2:
+		std::cout << FebruaryDay;
+		break;
+	case 3:
+		std::cout << "31";
+		break;
+	case 4:
+		std::cout << "30";
+		break;
+	case 5:
+		std::cout << "31";
+		break;
+	case 6:
+		std::cout << "30";
+		break;
+	case 7:
+		std::cout << "31";
+		break;
+	case 8:
+		std::cout << "31";
+		break;
+	case 9:
+		std::cout << "30";
+		break;
+	case 10:
+		std::cout << "31";
+		break;
+	case 11:
+		std::cout << "30";
+		break;
+	case 12:
+		std::cout << "31";
+		break;
+
+	default:
+        std::cout << "error";
+	}
+	return 0;
+}
