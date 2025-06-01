b6b78ff41b7abe3f38fb064853b21a470707b186
Yencry <yencry@qq.com>
2025-06-01 12:33:37 +0800

    Add files via upload

---
+++ 【入门1】顺序结构/B2005 字符三角形.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+	char a;char b= ' ';
+	std::cin >> a;
+	std::cout << b << b << a << std::endl;
+	std::cout << b << a << a << a << std::endl;
+	std::cout << a << a << a << a << a << std::endl;
+}
