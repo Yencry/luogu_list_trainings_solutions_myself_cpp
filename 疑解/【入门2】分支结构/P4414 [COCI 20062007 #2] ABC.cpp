64836d0919f95727b5f39bc81bea797fe93d4e6e
Yecnry <214315982+Yencry@users.noreply.github.com>
2025-06-05 10:22:32 +0800

    update files

---
+++ 疑解/【入门2】分支结构/P4414 [COCI 20062007 #2] ABC.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <array>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <ranges> 
+
+using namespace std;
+namespace
+{
+	
+	int readIn(){
+		int a;
+		cin >> a ;
+		return a;
+	}
+}
+
+int main() {
+	char command[] = { 'A', 'B', 'C' };	// A:0, B:1, C:2
+	array<int,3> numbers;
+	for (auto& in : numbers) in = readIn();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	string litter;getline(cin, litter);
+	std::ranges::sort(numbers);
+	int i = 0;
+	for (char ch : litter)
+	{
+		const auto index = std::ranges::find(command, ch) - begin(command);
+		cout << numbers[index];
+		if (i < 2) cout << " ";
+	}
+	return 0;
+}
