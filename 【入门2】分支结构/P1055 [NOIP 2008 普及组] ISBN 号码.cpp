94f3bb114aceb74d15c2a89958fdc7959739f3d0
Yecnry <214315982+Yencry@users.noreply.github.com>
2025-06-06 12:30:43 +0800

    update

---
+++ 【入门2】分支结构/P1055 [NOIP 2008 普及组] ISBN 号码.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	string isbn;
+	getline(cin, isbn);
+	int weight = 1;int sum = 0;
+	for (size_t i = 0; i < isbn.size() && weight <= 9; ++i) {
+		if (isdigit(isbn[i])) {
+			sum += (isbn[i] - '0') * weight;
+			++weight;
+		}
+	}
+	if (sum % 11 == 10){
+		if (isbn.back() == 'X')
+		{
+			cout << "Right";
+			return 0;
+		}
+		isbn.back() = 'X';
+		cout << isbn;
+		return 0;
+	}
+	if (const int result = sum % 11;
+		result == isbn.back() - '0') cout << "Right";
+	else{
+		isbn.back() = static_cast<char>(result + '0');
+		cout << isbn;
+	}
+	return 0;
+	}
